c30343998af8e1fa96f25b999205804dbfa3eedb
paullla <paula.cucuk@gmail.com>
2016-02-08 10:15:42 +0100

    added all api methods

--- src/Locastic/HpExpressApi/Api.php
+++ src/Locastic/HpExpressApi/Api.php
@@ -38,6 +38,176 @@ class Api
     }
 
     /**
+     * @param CreateShipmentOrdersResponse $createShipmentOrdersResponse
+     *
+     * @return mixed|string
+     */
+    public function createShipmentOrdersResponse(CreateShipmentOrdersResponse $createShipmentOrdersResponse)
+    {
+        return $this->sendRequest('CreateShipmentOrdersResponse', $createShipmentOrdersResponse);
+    }
+
+    /**
+     * @param GetAdditionalServices $getAdditionalServices
+     *
+     * @return mixed|string
+     */
+    public function getAdditionalServices(GetAdditionalServices $getAdditionalServices)
+    {
+        return $this->sendRequest('GetAdditionalServices', $getAdditionalServices);
+    }
+
+    /**
+     * @param GetAdditionalServicesResponse $getAdditionalServicesResponse
+     *
+     * @return mixed|string
+     */
+    public function getAdditionalServicesResponse(GetAdditionalServicesResponse $getAdditionalServicesResponse)
+    {
+        return $this->sendRequest('GetAdditionalServicesResponse', $getAdditionalServicesResponse);
+    }
+
+    /**
+     * @param GetBillOfLadingScans $getBillOfLadingScans
+     *
+     * @return mixed|string
+     */
+    public function getBillOfLadingScans(GetBillOfLadingScans $getBillOfLadingScans)
+    {
+        return $this->sendRequest('GetBillOfLadingScans', $getBillOfLadingScans);
+    }
+
+    /**
+     * @param GetBillOfLadingScansResponse $getBillOfLadingScansResponse
+     *
+     * @return mixed|string
+     */
+    public function getBillOfLadingScansResponse(GetBillOfLadingScansResponse $getBillOfLadingScansResponse)
+    {
+        return $this->sendRequest('GetBillOfLadingScansResponse', $getBillOfLadingScansResponse);
+    }
+
+    /**
+     * @param GetCashPaymentTypes $getCashPaymentTypes
+     *
+     * @return mixed|string
+     */
+    public function getCashPaymentTypes(GetCashPaymentTypes $getCashPaymentTypes)
+    {
+        return $this->sendRequest('GetCashPaymentTypes', $getCashPaymentTypes);
+    }
+
+    /**
+     * @param GetCashPaymentTypesResponse $getCashPaymentTypesResponse
+     *
+     * @return mixed|string
+     */
+    public function getCashPaymentTypesResponse(GetCashPaymentTypesResponse $getCashPaymentTypesResponse)
+    {
+        return $this->sendRequest('GetCashPaymentTypesResponse', $getCashPaymentTypesResponse);
+    }
+
+    /**
+     * @param GetContents $getContents
+     *
+     * @return mixed|string
+     */
+    public function getContents(GetContents $getContents)
+    {
+        return $this->sendRequest('GetContents', $getContents);
+    }
+
+    /**
+     * @param GetContentsResponse $getContentsResponse
+     *
+     * @return mixed|string
+     */
+    public function getContentsResponse(GetContentsResponse $getContentsResponse)
+    {
+        return $this->sendRequest('GetContentsResponse', $getContentsResponse);
+    }
+
+    /**
+     * @param GetDeliveryTypes $getDeliveryTypes
+     *
+     * @return mixed|string
+     */
+    public function getDeliveryTypes(GetDeliveryTypes $getDeliveryTypes)
+    {
+        return $this->sendRequest('GetDeliveryTypes', $getDeliveryTypes);
+    }
+
+    /**
+     * @param GetDeliveryTypesResponse $getDeliveryTypesResponse
+     *
+     * @return mixed|string
+     */
+    public function getDeliveryTypesResponse(GetDeliveryTypesResponse $getDeliveryTypesResponse)
+    {
+        return $this->sendRequest('GetDeliveryTypesResponse', $getDeliveryTypesResponse);
+    }
+
+    /**
+     * @param GetMainServices $getMainServices
+     *
+     * @return mixed|string
+     */
+    public function getMainServices(GetMainServices $getMainServices)
+    {
+        return $this->sendRequest('GetMainServices', $getMainServices);
+    }
+
+    /**
+     * @param GetMainServicesResponse $getMainServicesResponse
+     *
+     * @return mixed|string
+     */
+    public function getMainServicesResponse(GetMainServicesResponse $getMainServicesResponse)
+    {
+        return $this->sendRequest('GetMainServicesResponse', $getMainServicesResponse);
+    }
+
+    /**
+     * @param GetPackageScans $getPackageScans
+     *
+     * @return mixed|string
+     */
+    public function getPackageScans(GetPackageScans $getPackageScans)
+    {
+         return $this->sendRequest('GetPackageScans', $getPackageScans);
+    }
+
+    /**
+     * @param GetPackageScans $getPackageScans
+     *
+     * @return mixed|string
+     */
+    public function getPackageScansResponse(GetPackageScans $getPackageScans)
+    {
+        return $this->sendRequest('GetPackageScansResponse', $getPackageScans);
+    }
+
+    /**
+     * @param GetPaymentTypes $getPaymentTypes
+     *
+     * @return mixed|string
+     */
+    public function getPaymentTypes(GetPaymentTypes $getPaymentTypes)
+    {
+        return $this->sendRequest('GetPaymentTypes', $getPaymentTypes);
+    }
+
+    /**
+     * @param GetPaymentTypesResponse $getPaymentTypesResponse
+     *
+     * @return mixed|string
+     */
+    public function getPaymentTypesResponse(GetPaymentTypesResponse $getPaymentTypesResponse)
+    {
+        return $this->sendRequest('GetPaymentTypesResponse', $getPaymentTypesResponse);
+    }
+
+    /**
      * @param GetShipmentOrders $getShipmentOrders
      *
      * @return mixed|string
@@ -48,13 +218,53 @@ class Api
     }
 
     /**
-     * @param GetPackageScans $getPackageScans
+     * @param GetShipmentOrdersResponse $getShipmentOrdersResponse
      *
      * @return mixed|string
      */
-    public function getPackageScans(GetPackageScans $getPackageScans)
+    public function getShipmentOrdersResponse(GetShipmentOrdersResponse $getShipmentOrdersResponse)
     {
-        return $this->sendRequest('GetPackageScans', $getPackageScans);
+        return $this->sendRequest('GetShipmentOrdersResponse', $getShipmentOrdersResponse);
+    }
+
+    /**
+     * @param WSCreateShipmentOrdersResponse $WSCreateShipmentOrdersResponse
+     *
+     * @return mixed|string
+     */
+    public function WSCreateShipmentOrdersResponse(WSCreateShipmentOrdersResponse $WSCreateShipmentOrdersResponse)
+    {
+        return $this->sendRequest('WSCreateShipmentOrdersResponse', $WSCreateShipmentOrdersResponse);
+    }
+
+    /**
+     * @param WSGetBillOfLadingScansResponse $WSGetBillOfLadingScansResponse
+     *
+     * @return mixed|string
+     */
+    public function  WSGetBillOfLadingScansResponse(WSGetBillOfLadingScansResponse $WSGetBillOfLadingScansResponse)
+    {
+        return $this->sendRequest('WSGetBillOfLadingScansResponse', $WSGetBillOfLadingScansResponse);
+    }
+
+    /**
+     * @param WSGetPackageScansResponse $WSGetPackageScansResponse
+     *
+     * @return mixed|string
+     */
+    public function WSGetPackageScansResponse(WSGetPackageScansResponse $WSGetPackageScansResponse)
+    {
+        return $this->sendRequest('WSGetPackageScansResponse', $WSGetPackageScansResponse);
+    }
+
+    /**
+     * @param WSGetShipmentOrdersResponse $WSGetShipmentOrdersResponse
+     *
+     * @return mixed|string
+     */
+    public function WSGetShipmentOrdersResponse(WSGetShipmentOrdersResponse $WSGetShipmentOrdersResponse)
+    {
+        return $this->sendRequest('WSGetShipmentOrdersResponse', $WSGetShipmentOrdersResponse);
     }
 
     /**
